57d2b0d7f456f2b44aa4afe49d0c2f5c1be40e24
Isaac Cohen <issc29@gmail.com>
2023-02-27 23:30:38 -0500

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const collection = require('./collection'); // any Postman collection JSON file
+const { transpile } = require('postman2openapi');
+
+
+try {
+const myToken = core.getInput('myToken');
+const context = github.context
+const octokit = github.getOctokit(myToken)
+
+const content = await octokit.rest.repos.getContent({
+    ...context.repo,
+    path: 'postman/collections'
+  });
+
+  console.log(content)
+
+
+
+// Returns a JavaScript object representation of the OpenAPI definition.
+//const openapi = transpile(collection);
+
+//console.log(JSON.stringify(openapi, null, 2));
+
+
+} catch (error) {
+  core.setFailed(error.message);
+}
